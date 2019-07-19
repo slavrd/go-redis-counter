3d559f5b99572858052a7bb29ca4739219f1c744
Slav Donchev <slav.donchev@gmail.com>
2019-07-19 16:03:57 +0300

    add initial counter code

---
+++ rediscounter.go
@@ -0,0 +1,71 @@
+// Package rediscounter provides a counter that stores its value in a redis server
+package rediscounter
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/go-redis/redis"
+)
+
+// RedisCounter represents a counter that stores its value in redis
+type RedisCounter struct {
+	rkey    string
+	rclient *redis.Client
+}
+
+// Get returns the current value counter
+func (rc *RedisCounter) Get() (int, error) {
+
+	result, err := rc.rclient.Get(rc.rkey).Result()
+
+	// set rkey to 0 if it does not exist in redis
+	if err == redis.Nil {
+		_, serr := rc.rclient.Set(rc.rkey, 0, 0).Result()
+		if serr != nil {
+			return 0, fmt.Errorf("error initializing redis key: %v", serr)
+		}
+	} else if err != nil {
+		return 0, fmt.Errorf("error reading form reids: %v", err)
+	}
+
+	// confirm that the value is int and return it
+	cv, err := strconv.Atoi(result)
+	if err != nil {
+		return 0, fmt.Errorf("redis key: %q value: %q is not int: %v",
+			rc.rkey, result, err)
+	}
+	return cv, nil
+}
+
+// IncrBy increases the counter's value by "a" amount and reports the resulting value
+func (rc *RedisCounter) IncrBy(a int64) (int64, error) {
+	rv, err := rc.rclient.IncrBy(rc.rkey, a).Result()
+	if err != nil {
+		return 0, fmt.Errorf("error incrementing value: %v", err)
+	}
+	return rv, nil
+}
+
+// NewCounter returns creates a RedisCounter with the provided connection details.
+// raddr is host:port address.
+func NewCounter(raddr, rpass, rkey string, rdb int) (*RedisCounter, error) {
+
+	// create the redis client and check it can connect
+	rclient := redis.NewClient(&redis.Options{
+		Addr:     raddr,
+		DB:       rdb,
+		Password: rpass,
+	})
+
+	// Check if rkey exist and create it if it does not.
+	_, err := rclient.Get(rkey).Result()
+	if err == redis.Nil {
+		_, err = rclient.Set(rkey, 0, 0).Result()
+	}
+	if err != nil {
+		return nil, fmt.Errorf("failed connecting to redis: %v", err)
+	}
+
+	return &RedisCounter{rclient: rclient, rkey: rkey}, nil
+}
