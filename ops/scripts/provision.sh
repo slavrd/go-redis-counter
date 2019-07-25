488992a9ffa6601b357fc4d5fc01f940c0eb74ea
Slav Donchev <slav.donchev@gmail.com>
2019-07-25 09:22:36 +0300

    add Vagrant configuration

---
+++ ops/scripts/provision.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+# provisions the VM with the needed packages
+
+# list of packages to install, separated by space
+PKGS="git vim"
+
+# version of Golang to install
+GOVER="1.12"
+
+which $PKGS || {
+    sudo apt-get update
+    sudo apt-get install -y $PKGS
+}
+
+# download and run golang install script
+wget -q -P /tmp https://raw.githubusercontent.com/slavrd/bash-various-scripts/master/install-golang.sh && \
+    bash /tmp/install-golang.sh 1.12 linux amd64
