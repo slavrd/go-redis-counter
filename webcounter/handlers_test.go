0970d50bb5a460b7f09b71233e2b1b36f2749be3
Slav Donchev <slav.donchev@gmail.com>
2019-08-14 15:53:15 +0300

    add initial go tests

---
+++ webcounter/handlers_test.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+// TestHandlers runs testcases over the server handlers.
+func TestHandlers(t *testing.T) {
+
+	// define testcases
+	tests := []struct {
+		handler  func(w http.ResponseWriter, r *http.Request)
+		reqPath  string
+		wantCode int
+		ctrDif   int64
+	}{
+		{
+			handler:  handleGet,
+			reqPath:  "/get",
+			wantCode: 200,
+			ctrDif:   0,
+		},
+		{
+			handler:  handleIncr,
+			reqPath:  "/incr",
+			wantCode: 200,
+			ctrDif:   1,
+		},
+	}
+
+	// execute tests
+	for _, test := range tests {
+		initCtrValue := htmlCounterCtx.CtrValue
+		initTime := htmlCounterCtx.Time
+
+		r := httptest.NewRequest("GET", test.reqPath, nil)
+		w := httptest.NewRecorder()
+
+		test.handler(w, r)
+
+		if w.Code != test.wantCode {
+			t.Errorf("handler testcase:\n%v\nwrong status code, want: %v, got: %v", test, test.wantCode, w.Code)
+		}
+
+		if initCtrValue+test.ctrDif != htmlCounterCtx.CtrValue {
+			t.Errorf("handler testcase:\n%v\nincreased the counter value. Initial value: %v current value: %v", test, initCtrValue, htmlCounterCtx.CtrValue)
+		}
+
+		if htmlCounterCtx.Time.Sub(initTime) >= 0 {
+			t.Errorf("handler testcase:\n%v\nnot update Time correctly. Initial value: %v current value: %v", test, initTime, htmlCounterCtx.Time)
+		}
+
+		// check the response body
+		tpl, err := loadTemplate(*tplPath)
+		if err != nil {
+			t.Fatalf("error setting up response body check: %v", err)
+		}
+		buf := bytes.NewBuffer(make([]byte, 0))
+		tpl.Execute(buf, htmlCounterCtx)
+
+		if !bytes.Equal(buf.Bytes(), w.Body.Bytes()) {
+			t.Errorf("handleGet: wrong body \nwant:\n\n%s\n\ngot:\n\n%s\n", buf.String(), w.Body.String())
+		}
+	}
+}
---
+++ webcounter/main_test.go
@@ -0,0 +1,82 @@
+// Contains the test setup
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/go-redis/redis"
+)
+
+// redis addres to use for the tests
+var raddr = "127.0.0.1:6379"
+
+// redis password to use for tests
+var rpass = os.Getenv("REDIS_PASS")
+
+// redis key to use for the tests
+var rkey = "count"
+
+// redis db to use for the tests
+var rdb = 10
+
+// a redis.Client to use for setting up redis test values
+var c *redis.Client
+
+// commnad line flags
+var clearRKey = flag.Bool("d", false, "clear redis key if already present")
+
+func init() {
+
+	flag.Parse()
+
+	// normalize redis address
+	envRAddr := os.Getenv("REDIS_ADDR")
+	if envRAddr != "" {
+		raddr = envRAddr
+	} else {
+		raddr = *redisHost
+	}
+
+	if !strings.ContainsRune(raddr, ':') {
+		raddr = strings.Join([]string{raddr, strconv.Itoa(*redisPort)}, ":")
+	}
+
+	// create a redis.Client to interract with redis
+	c = redis.NewClient(&redis.Options{
+		Addr:     raddr,
+		DB:       rdb,
+		Password: rpass,
+	})
+
+	// confirm the client is working
+	_, err := c.Ping().Result()
+	if err != nil {
+		log.Fatalf("error creating redis.Client: %v", err)
+	}
+
+	// check if key is already present
+	_, err = c.Get(rkey).Result()
+	if err != redis.Nil {
+		log.Printf("WARN: key %q already exist in redis db %v", rkey, rdb)
+		if !*clearRKey {
+			log.Fatal("aborting tests: can pass -d flag to allow existing key modification")
+		}
+	}
+
+	// set up tmlCounterCtx
+	htmlCounterCtx, err = newHTMLCounter(raddr, rpass, rkey, rdb)
+	if err != nil {
+		log.Fatalf("error createting htmlCounter instance for htmlCounterCtx: %v", err)
+	}
+
+	// set up htmlCounterTpl
+	htmlCounterTpl, err = loadTemplate(*tplPath)
+	if err != nil {
+		log.Fatalf("error loading html template: %v", err)
+	}
+
+}
