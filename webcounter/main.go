779e9e883134269c82b5cb2f80a037ef6369a0a6
Slav Donchev <slav.donchev@gmail.com>
2019-08-26 13:51:56 +0300

    add metrics code

--- webcounter/main.go
+++ webcounter/main.go
@@ -13,13 +13,15 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	rediscounter "github.com/slavrd/go-redis-counter"
 )
 
 // command line flags
 var bindAddr = flag.String("bind-addr", "0.0.0.0:8000", "server bind address")
-var tplPath = flag.String("html-tpl", "html/index.gohtml", "path to the golang html template to use")
+var tplPath = flag.String("html-tpl", "html/index.gohtml", "path to the golang html template to use for main page")
+var mtplPath = flag.String("html-mtpl", "html/metrics.gohtml", "path to the golang html template to use for metrics page")
 var redisHost = flag.String("redis-host", "127.0.0.1", "redis server host address")
 var redisPort = flag.Int("redis-port", 6379, "redis server port")
 var redisPass = flag.String("redis-pass", "", "redis server password")
@@ -35,7 +37,9 @@ var vSecretKey = flag.String("vault-secret-key", "pass", "vault secret key for r
 var redisAddr string     // host:port address for the redis server
 var redisConnInfo string // string to display on the web page
 var counter *rediscounter.RedisCounter
-var htmlCounterTpl *template.Template // html template to render for responses
+var htmlCounterTpl *template.Template // html template to render counter
+var htmlMetricsTpl *template.Template // html template to render metrics data
+var usageData *metrics
 
 func init() {
 	flag.Parse()
@@ -72,10 +76,17 @@ func init() {
 	var err error
 	htmlCounterTpl, err = loadTemplate(*tplPath)
 	if err != nil {
-		log.Fatalf("error loading html template: %v", err)
+		log.Fatalf("error loading index html template: %v", err)
+	}
+
+	htmlMetricsTpl, err = loadMetricsTpl(*mtplPath)
+	if err != nil {
+		log.Fatalf("error loading metrics html template: %v", err)
 	}
 
 	redisConnInfo = fmt.Sprintf("redis @ %s, db: %v, key: %q", redisAddr, *redisDB, *redisKey)
+
+	usageData = newMetrics(redisConnInfo)
 }
 
 func main() {
@@ -92,6 +103,7 @@ func main() {
 	http.Handle("/incr", newHandler(newIncrCtx, htmlCounterTpl))
 	http.Handle("/get", newHandler(newGetCtx, htmlCounterTpl))
 	http.Handle("/health", newHealthHandler(counter.RedisHealth))
+	http.Handle("/metrics", newMetricsHandler(usageData, htmlMetricsTpl))
 
 	// start server
 	log.Fatal(http.ListenAndServe(*bindAddr, nil))
@@ -113,11 +125,30 @@ func loadTemplate(tplPath string) (*template.Template, error) {
 	return tpl, nil
 }
 
+// loadMetricsTpl reads the file from mtplPath and parses it as a go html template
+func loadMetricsTpl(mtplPath string) (*template.Template, error) {
+	content, err := ioutil.ReadFile(mtplPath)
+	if err != nil {
+		return nil, err
+	}
+
+	tpl, err := template.New("metrics").
+		Funcs(template.FuncMap{"timenow": time.Now}).
+		Parse(string(content))
+	if err != nil {
+		return nil, err
+	}
+
+	return tpl, nil
+}
+
 // newHandler returns a http.Handler func which renders the tpl template
 // with a counterCtx incstace created using cf func
 func newHandler(ctxf func(*rediscounter.RedisCounter) (*counterCtx, error), tpl *template.Template) http.Handler {
 
 	h := func(w http.ResponseWriter, r *http.Request) {
+
+		usageData.add(r.URL.Path)
 
 		ctx, err := ctxf(counter)
 		if err != nil {
@@ -136,9 +167,14 @@ func newHandler(ctxf func(*rediscounter.RedisCounter) (*counterCtx, error), tpl 
 	return http.HandlerFunc(h)
 }
 
+// newHealthHandler returns a http.Handler func which will call the hcf func and
+// will return 200 OK if result is nil or 500 Internal Server Error if result is an error
 func newHealthHandler(hcf func() error) http.Handler {
 
 	h := func(w http.ResponseWriter, r *http.Request) {
+
+		usageData.add(r.URL.Path)
+
 		err := hcf()
 		if err == nil {
 			w.WriteHeader(200)
@@ -148,6 +184,26 @@ func newHealthHandler(hcf func() error) http.Handler {
 			log.Printf("error healthcheck: %v", err)
 			w.Write([]byte("Redis server is down!"))
 		}
+	}
+
+	return http.HandlerFunc(h)
+}
+
+// newMetricsHandler returns a http.Handler func which will render the provided tpl
+// with the m context. It will respect the m.Mutex so will be concurency safe.
+func newMetricsHandler(m *metrics, tpl *template.Template) http.Handler {
+
+	h := func(w http.ResponseWriter, r *http.Request) {
+
+		m.add(r.URL.Path)
+
+		m.Mutex <- struct{}{}
+		err := tpl.Execute(w, m)
+		<-m.Mutex
+		if err != nil {
+			log.Printf("error writing response: %v", err)
+		}
+
 	}
 
 	return http.HandlerFunc(h)
