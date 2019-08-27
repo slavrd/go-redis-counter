4befa65f554c9dd6b064e926d4410ff69462e89b
Slav Donchev <slav.donchev@gmail.com>
2019-08-27 14:37:11 +0300

    update acceptance tests

--- tests/test_rc_webcounter.sh
+++ tests/test_rc_webcounter.sh
@@ -3,3 +3,6 @@
 
 curl -sS localhost:8000/get | grep -E '<div id="countervalue">[0-9]+</div>' || exit 1
 curl -sS localhost:8000/incr | grep -E '<div id="countervalue">[0-9]+</div>' || exit 1
+curl -sS localhost:8000/health | grep -E 'OK' || exit 1
+curl -sS localhost:8000/reset | grep -E '<div id="countervalue">0</div>' || exit 1
+curl -sS localhost:8000/metrics | grep -E '<tr><td>/metrics</td><td>1</td></tr>' || exit 1
